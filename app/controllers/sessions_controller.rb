2a12465220c4168ed42569e55470d3bed579bc0e
Luis Garcia <garcia@yahoo.com>
2018-03-25 12:14:09 -0600

    Create variables to hold corresponding attributes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
 
   def create
+    client_id     = '8034c16884a580b037a2'
+    client_secret = '3a498e62fc6eb97d074267e3927c688430299714'
+    code          = params[:code]
+    response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+
   end
 
 end
